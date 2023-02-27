c50a7c4689259c0322897d50e87b201505d92dc5
CybertronicEvidence <adejuwonevidence181@gmail.com>
2023-02-27 08:35:38 +0100

    tested chat for errors

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -70,10 +70,10 @@ const Chat = () => {
       </div>}
       <div>
 
-        <div className='bg-red-500 h-[10vh] flex justify-center items-center'><a href='https://www.hapsie.com/'><img src={hapsie} alt='logo' style={{height: "80px"}}/></a></div>
+        <div className='bg-red-500 h-[10vh] flex justify-center items-center'><a href='https://www.hapsie.com/'><img src={hapsie} alt='logo' style={{ height: "80px" }} /></a></div>
 
         <div className='flex'>
-          <div style={{ borderRight:"2px solid rgb(59 130 246)"}} className='bg-red-500 w-[30%] h-[80vh] px-4 hidden md:block'>
+          <div style={{ borderRight: "2px solid rgb(59 130 246)" }} className='bg-red-500 w-[30%] h-[80vh] px-4 hidden md:block'>
             <div className='h-[50%] overflow-hidden'><img src={cunningfull} alt='cunningfull' /></div>
             <div>
               <p className='text-white'><span className='font-semibold'>Disclaimer for Parents:</span> This Cunning Carly chatbot is automated and is not monitored by a human in real-time. It has been trained on the latest AI to help answer questions and give guidance about bettering the environment. We have made every effort to stop it providing dangerous and inappropriate information or responses but exceptions can happen. We only suggest children use this with moderation of an adult. Any issues please email <a href='mailto:abuse@hapsie.com' >abuse@hapsie.com</a> immediately.</p>
@@ -81,7 +81,7 @@ const Chat = () => {
           </div>
           <div className='bg-white w-full md:w-[70%] h-[80vh] flex flex-col justify-between'>
             <div className='h-[80vh] flex justify-center bg-white relative'>
-              <div className='w-full px-[4%] overflow-y-auto absolute bottom-0' style={{maxHeight:'100%'}}>
+              <div className='w-full px-[4%] overflow-y-auto absolute bottom-0' style={{ maxHeight: '100%' }}>
                 {chatLog.map((message, index) => (
                   <ChatMessage key={index} message={message} />
                 ))}
@@ -101,7 +101,7 @@ const Chat = () => {
             <div className='h-[10vh] py-4 bg-white'>
               <form onSubmit={handleSubmit} className='flex justify-center'>
                 <div className='flex justify-center items-center bg-white w-[80%] border-2 border-blue-500 rounded-xl pr-2'>
-                  <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Say hi or ask a question!' className='w-[100%] h-[40px] rounded-xl px-2' style={{outline: 'none'}}/>
+                  <input type='text' value={input} onChange={(e) => setInput(e.target.value)} placeholder='Say hi or ask a question!' className='w-[100%] h-[40px] rounded-xl px-2' style={{ outline: 'none' }} />
                   <button type='submit'><BiSend size='1.8rem' className='text-blue-500' /></button>
                 </div>
               </form>
@@ -115,10 +115,10 @@ const Chat = () => {
         <div className='bg-red-500 h-[10vh] justify-evenly gap-4 flex md:hidden items-center w-full'>
           <div><a href='https://www.hapsie.com/'><img src={hapsie} width={100} height={70} alt='logo' /></a></div>
           <div><p onClick={() => setChatLog([...chatLog,
-            { 
-              user: 'gpt', 
-              message: 'I am here to help you with questions and answers about the environment, sustainable and recycling. But I can also do fun creative tasks too! For example, I can write poems, lesson plans, to do lists. I can give you step by step guides how to arrange beach cleans (for example). Talk to me and let me help you better the world around you!' 
-            }
+          {
+            user: 'gpt',
+            message: 'I am here to help you with questions and answers about the environment, sustainable and recycling. But I can also do fun creative tasks too! For example, I can write poems, lesson plans, to do lists. I can give you step by step guides how to arrange beach cleans (for example). Talk to me and let me help you better the world around you!'
+          }
           ])} className='text-center w-full text-white'>What Can I Do?</p></div>
           <div className='flex gap-2 text-center text-white' onClick={() => setShowModal(true)}><p>Disclaimer</p></div>
         </div>
